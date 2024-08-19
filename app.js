1ac944a7964151fa50d08928cb6dd1a6081f8600
Massamba505 <sabelomaphalala131@gmail.com>
2024-08-20 00:12:37 +0200

    dummy

--- app.js
+++ app.js
@@ -7,19 +7,11 @@ const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth.route');
 const userRouter = require('./routes/user.route');
 const eventRouter = require('./routes/event.route');
-var ticketRouter = require('./routes/tickets');
+const ticketRouter = require('./routes/tickets');
 
 const app = express();
 const cors = require('cors');
 app.use(cors());
-    // app.use(function (req, res, next) {
-    // //Enabling CORS
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
-    // Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    //   next();
-    // });
 
 app.use(logger('dev'));
 app.use(express.json());
